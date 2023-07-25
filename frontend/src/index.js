cd5d76e9f86ffa7117a47e3ee6d70fd3024c4b65
M-Bahy <Homelander.SolderBoy@gmail.com>
2023-07-25 11:40:31 +0300

    removed unnesesary imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import About from './about';
 import Navbar from './Navbar';
 
@@ -27,4 +26,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
